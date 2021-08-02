2b0f4b3415674544ec1b8f03d969e35dfc02026c
Kristian Puno <punokristian@yahoo.com>
2021-08-02 08:48:26 +0800

    Removed ads. Added fight log table.

--- public/js/fight-logger.js
+++ public/js/fight-logger.js
@@ -1,9 +1,23 @@
 let subs = []
 let txs = []
 let fightInterval = 10 //seconds
+var currCurrency = 'php'
+
+var skillPrice = 0
+var localPrice = 0
+var bnbPrice = 0
+var usdPrice = 0
+
+$('document').ready(async () => {
+    priceTicker()
+    setInterval(() => {
+        priceTicker()
+    }, 30000)
+})
 
 const fightAddress = $('#fight-address')
-const fightResult = $('#fight-result')
+var fightResult = []
+var $table = $('#table-accounts tbody')
 
 async function subscribe (address) {
     console.log('Subscribed:', address)
@@ -24,7 +38,18 @@ async function subscribe (address) {
                         const tx = await getTransaction(result.transactionHash)
                         const receipt = await getTransactionReceipt(result.transactionHash)
                         const gasCost = tx.gasPrice * receipt.gasUsed
-                        fightResult.append(`${owner},${(parseInt(playerRoll) > parseInt(enemyRoll) ? 'Win' : 'Lost')},${character},${weapon},${playerRoll},${enemyRoll},${web3.utils.fromWei(BigInt(skillGain).toString(), 'ether')},${xpGain},${web3.utils.fromWei(BigInt(gasCost).toString(), 'ether')}\n`)
+                        // fightResult.append(`${owner},${(parseInt(playerRoll) > parseInt(enemyRoll) ? 'Win' : 'Lost')},${character},${weapon},${playerRoll},${enemyRoll},${web3.utils.fromWei(BigInt(skillGain).toString(), 'ether')},${xpGain},${web3.utils.fromWei(BigInt(gasCost).toString(), 'ether')}\n`)
+                        const charData = await characterFromContract(character, await getCharacterData(character))
+                        temp = {
+                            "Name" : NaN,
+                            "Address" : owner,
+                            "Level" : charData.level + 1,
+                            "Result" : (parseInt(playerRoll) > parseInt(enemyRoll) ? 'Win' : 'Lost'),
+                            "Reward" : web3.utils.fromWei(BigInt(skillGain).toString(), 'ether'),
+                            "Gas" : web3.utils.fromWei(BigInt(gasCost).toString(), 'ether'),
+                        }
+                        fightResult.push(temp)
+                        await loadData()
                         txs.push(result.transactionHash)
                     }
                 })
@@ -35,15 +60,85 @@ async function subscribe (address) {
     }, 1000)
 }
 
+function toLocaleCurrency(val) {
+    return val.toLocaleString('en-US', { style: 'currency', currency: currCurrency.toUpperCase() })
+}
+function convertToFiat (val) {
+    return parseFloat(val) * localPrice
+}
+function convertBnbToFiat (val) {
+    return parseFloat(val) * bnbPrice
+}
+async function priceTicker() {
+    $.get(`https://api.coingecko.com/api/v3/simple/price?ids=cryptoblades,binancecoin,tether&vs_currencies=usd,php`, (result) => {
+        skillPrice = result.cryptoblades['usd']
+        localPrice = result.cryptoblades[currCurrency]
+        bnbPrice = result.binancecoin[currCurrency]
+        usdPrice = result.tether[currCurrency]
+    })
+}
+
 async function addAccount() {
     var address = $('#logger-address').val().trim()
     if (!Object.keys(subs).includes(address) && isAddress(address)) {
-        await subscribe(address)        
+        await subscribe(address)
         fightAddress.append(`${address}\n`)
         $('#modal-add-account').modal('hide')
+        // test()
+        // await loadData()
     }
 }
 
+function test(){
+    temp = {
+        "Name" : "",
+        "Address" : "test",
+        "Level" : 25,
+        "Result" : 'Win',
+        "Reward" : '2',
+        "Gas" : '1',
+    }
+    fightResult.push(temp)
+}
+async function loadData() {
+    $totalElement = $table.children().last()
+    totalSkill = 0
+    totalProfit = 0
+    totalGas = 0
+    fights = 0
+    wins = 0
+    $table.html('');
+    const fRowHtml = await Promise.all(fightResult.map(async (fight, i) => {
+        let rowHtml = ''
+        skill = convertToFiat(parseFloat(fight['Reward']))
+        gas = convertBnbToFiat(parseFloat(fight['Gas']))
+        profit = skill - gas
+        totalSkill += parseFloat(fight['Reward'])
+        totalGas += parseFloat(fight['Gas'])
+        totalProfit += profit
+        fights += 1
+        wins += fight['Result'] === "Win" ? 1 : 0
+        rowHtml += ` <tr class="text-white align-middle" data-row="${i}">
+                                <td rowspan="1" class='align-middle'>${fight['Name']}</td>
+                                <td rowspan="1" class='align-middle'>${fight['Address'].substr(0, 6)}...${fight['Address'].substr(-4, 4)}</td>
+                                <td rowspan="1" class='align-middle'>Lv. ${fight['Level']}</td>
+                                <td rowspan="1" class='align-middle'>${fight['Result']}</td>
+                                <td rowspan="1" class='align-middle'>${parseFloat(fight['Reward']).toFixed(6)} SKILL<br />${(Number(parseFloat(fight['Reward'])) > 0 ? `<span style="font-size: 10px;">(${toLocaleCurrency(skill)})</span>` : '')}</td>
+                                <td rowspan="1" class='align-middle'>${parseFloat(fight['Gas']).toFixed(6)} BNB<br />${(Number(parseFloat(fight['Gas'])) > 0 ? `<span style="font-size: 10px;">(${toLocaleCurrency(gas)})</span>` : '')}</td>
+                                <td rowspan="1" class='align-middle'>${toLocaleCurrency(profit)}</td>
+                            </tr>`;
+        return rowHtml
+    }))
+    $table.html(fRowHtml)
+    totalTotal = `<tr class="text-white align-middle">
+                    <td colspan="3" style="text-align: right">Total</td>
+                    <td rowspan="1" class='align-middle'>${wins}/${fights}</td>
+                    <td rowspan="1" class='align-middle'>${parseFloat(totalSkill).toFixed(6)} SKILL<br />${(Number(parseFloat(totalSkill)) > 0 ? `<span style="font-size: 10px;">(${toLocaleCurrency(convertToFiat(totalSkill))})</span>` : '')}</td>
+                    <td rowspan="1" class='align-middle'>${parseFloat(totalGas).toFixed(6)} BNB<br />${(Number(parseFloat(totalGas)) > 0 ? `<span style="font-size: 10px;">(${toLocaleCurrency(convertBnbToFiat(totalGas))})</span>` : '')}</td>
+                    <td rowspan="1" class='align-middle'>${toLocaleCurrency(totalProfit)}</td>
+                  </tr>`
+    $table.append(totalTotal)
+}
 function exportList() {
     var list = fightAddress.val().split('\n')
     list.splice(list.length-1, 1)
